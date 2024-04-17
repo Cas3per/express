76a71e3fa5999a26bd6ba359b947fcbd7557a213
Philemon Akpan <Philfriday6@gmail.com>
2024-04-17 11:58:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { isUtf8 } = require('buffer');
+const express = require('express');
+const app = express();
+const port = 9000
+const fs = require("fs");
+const path = require('path');
+const home = fs.readFileSync('./index.html', "utf-8")
+const product = fs.readFileSync('./product.html', 'utf-8')
+const about = fs.readFileSync('./about.html', 'utf-8')
+const contact = fs.readFileSync('./contact.html', 'utf-8')
+const testimonial = fs.readFileSync('./testimonial.html', 'utf-8')
+
+app.get('/', (req, res) =>{
+        res.end(home)
+        
+})
+
+app.get("/about", (req, res) =>{
+    res.send(about)
+})
+
+
+app.get("/contact", (req, res) =>{
+    res.send(contact)
+})
+
+
+app.get("/product", (req, res) =>{
+    res.send(product)
+})
+
+
+app.get("/testimonial", (req, res) =>{
+    res.send(testimonial)
+})
+
+app.listen(port, () => {
+    console.log(`server started succesfully. click http://localhost:${port} 
+    to open website`)
+})
